267bde97f9bb47a46a782c964c61b8da3297c70d
Jan Noha <nohajc@gmail.com>
2018-01-06 02:28:47 +0100

    make sure toHexString works in scala.js

--- src/main/scala/HexUtils.scala
+++ src/main/scala/HexUtils.scala
@@ -1,6 +1,6 @@
 object HexUtils {
   implicit class ArrayByteExt(arr: Array[Byte]) {
-    def toHexString(prefix: String): String = prefix + arr.map("%02x".format(_)).mkString
+    def toHexString(prefix: String): String = prefix + arr.map(x => f"${x & 0xFF}%02x").mkString
     def toHexString: String = toHexString("")
   }
 }
